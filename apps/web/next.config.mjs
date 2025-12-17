59c77c78e149c3d4efbf70c4af6a29ea23d94ac4
brian griffey <briangriffey@brians-MacBook-Air.local>
2025-12-16 20:51:36 -0600

    adding sourcemaps

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@meal-planner/database'],
+  productionBrowserSourceMaps: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
