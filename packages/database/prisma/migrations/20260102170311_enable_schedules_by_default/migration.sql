d65281ffe9bb84c7ab8162af87da557a1209e0fd
brian griffey <briangriffey@brians-MacBook-Air.local>
2026-01-02 17:09:02 -0600

    terrible datase name update

--- packages/database/prisma/migrations/20260102170311_enable_schedules_by_default/migration.sql
+++ packages/database/prisma/migrations/20260102170311_enable_schedules_by_default/migration.sql
@@ -1,6 +1,6 @@
 -- Enable schedules for all existing users who have it disabled
 -- This is a data migration to fix the issue where schedules were disabled by default
 
-UPDATE "UserPreferences"
+UPDATE "user_preferences"
 SET "scheduleEnabled" = true
 WHERE "scheduleEnabled" = false;
